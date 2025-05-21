865dddc676a6fd03586f383dcc0b2420c03c73a1
Elina1234567890 <ibragimovazhanel18@mail.ru>
2025-05-21 19:52:44 +0600

    Blacksmith strengh button

--- Assets/Scripts/Game/BlacksmithScript.cs
+++ Assets/Scripts/Game/BlacksmithScript.cs
@@ -1,10 +1,18 @@
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class BlacksmithScript : MonoBehaviour
 {
     [SerializeField] private GameObject _button;
     [SerializeField] private GameObject _BlacksmithPanel;
+
+
+    [SerializeField] private TMP_Text _textMeshPro;
+
+
+    private int _currentStrength = 10; 
+    private int _strengthIncrease = 10; 
     private void OnTriggerEnter2D(Collider2D other)
     {
 
@@ -14,8 +22,6 @@ public class BlacksmithScript : MonoBehaviour
         }
 
     }
-
-
 
     private void OnTriggerExit2D(Collider2D other)
     {
@@ -34,5 +40,17 @@ public class BlacksmithScript : MonoBehaviour
     public void BlacksmithExit()
     {
         _BlacksmithPanel.SetActive(false);
+    }
+
+    public void StrengthUpdate()
+    {
+        _currentStrength += _strengthIncrease;
+
+        UpdateStrengthText();
+    }
+
+    public void UpdateStrengthText()
+    {
+        _textMeshPro.text = $"Strength: {_currentStrength} + {_strengthIncrease}";
     }
 }
