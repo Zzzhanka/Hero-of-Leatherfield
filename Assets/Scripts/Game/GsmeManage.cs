1b836072cf9d2a246472a041a0acf7387dce40a2
Zhanik <ibragimovazhanel18@mail.ru>
2025-06-04 19:45:22 +0600

    fps & play button
    подняла минимальный фпс

---
+++ Assets/Scripts/Game/GsmeManage.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GsmeManage : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
